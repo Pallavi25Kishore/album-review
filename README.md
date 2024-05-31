ee5562be6cdd591a54297972c4b8c8c889e585de
unoriginal1234 <eric.t.roth@gmail.com>
2024-05-31 04:55:54 -0700

    Barebones repo setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# Create a CRUD-y Album Review App
+
+##  The page should show your reviewed albums on load
+
+### Each review should include:
+- The title of the album
+- A rating 1 through 5
+- A comment of max 250 characters
+
---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3001
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '../dist')))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
