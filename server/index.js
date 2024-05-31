b7abd53f90eb180e0c86de7811444a187ed6edbf
unoriginal1234 <eric.t.roth@gmail.com>
2024-05-31 09:07:43 -0700

    Changes prepped for students

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
-const path = require('path')
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
